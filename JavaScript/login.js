d11543f43efef6f0d4fdc868479cfddb9a83f23f
TanishqM1 <113214169+TanishqM1@users.noreply.github.com>
2025-03-23 02:34:28 -0700

    home page displays posts.

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -17,7 +17,7 @@ document.getElementById('login-form').addEventListener('submit', async function(
         
         if (data.success) {
             console.log('Logged in as:', data.username);
-            window.location.href = 'your-work.html';
+            window.location.href = 'home.html';
         } else {
             showError(data.message);
         }
